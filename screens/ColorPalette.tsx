25a0b218fd9eec5d31fc656a6544740d94fe518d
Aleksandra Kasprzak <aleksandra.kasprzak@espeo.eu>
2021-08-19 13:53:04 +0200

    do main

--- screens/ColorPalette.tsx
+++ screens/ColorPalette.tsx
@@ -8,7 +8,7 @@ export interface IColorPaletteProps {
   colors: { colorName: string; hexCode: string }[];
   paletteName: string;
 }
-
+console.log('commit na mainie');
 export const ColorPalette = () => {
   const route = useRoute<RootRouteProps<'ColorPalette'>>();
   console.log('new color palette');
